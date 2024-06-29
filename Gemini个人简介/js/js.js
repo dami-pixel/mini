aa6ff67468ad4e8d2cf350b91023908300f66d4c
dami-pixel <2351422987@qq.com>
2024-06-30 01:48:18 +0800

    Add files via upload

---
+++ Gemini个人简介/js/js.js
@@ -0,0 +1,3 @@
+window.onload=function(){		function hotChange(){		var current_index=0;		var timer=window.setInterval(autoChange, 3000);		var button_li=document.getElementById("button").getElementsByTagName("li");		var pic_li=document.getElementById("banner_pic").getElementsByTagName("li");		for(var i=0;i<button_li.length;i++){			button_li[i].onmouseover=function(){				if(timer){					clearInterval(timer);				}				for(var j=0;j<pic_li.length;j++){					if(button_li[j]==this){						current_index=j;						button_li[j].className="current";						pic_li[j].className="current";					}else{						pic_li[j].className="pic";						button_li[j].className="but";}	}	}			button_li[i].onmouseout=function(){				timer=setInterval(autoChange,3000);	}	}
+	
+	
